8aa8221ebee2fde0076b61a57c3f9b4059b2dad1
Roelof Briers <rbriers@gmail.com>
2019-12-08 22:18:09 +0200

    Add Day6 placeholder

---
+++ Day6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contentsMain = File.ReadAllText(Environment.CurrentDirectory + "/puzzle.txt");
+
+            Console.WriteLine($"{contentsMain}");
+        }
+    }
+}
