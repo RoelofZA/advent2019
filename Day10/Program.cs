5c1428d0a8cdd6b1a7323eb56a8a89338cec08c7
Roelof Briers <rbriers@gmail.com>
2019-12-24 21:23:41 +0200

    Updated with a few more days

---
+++ Day10/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace Day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contentsMain = File.ReadAllText(Environment.CurrentDirectory + "/puzzle04.txt");
+
+            string[] lines = contentsMain.Split("\n");
+            List<PointF> points = new List<PointF>(); 
+
+            // Get all Points
+            for (int y = 0; y < lines.Length; y++)
+            {
+                for (int i = 0; i < lines[y].Length; i++)
+                {
+                    if (lines[y][i]=='#')
+                    {
+                        points.Add(new PointF(i, y));
+                    }
+                }
+            }
+
+            int highestNumber = 0;
+
+
+            foreach (PointF current in points){
+                Hashtable ht = new Hashtable();
+                // Start Calculating
+                foreach (PointF item in points)
+                {
+                    if (item == current)
+                        continue;
+
+                    ht[angleOf(current, item)] = item;
+                }
+                //Console.WriteLine($"{current} {ht.Keys.Count}");
+                if (highestNumber < ht.Keys.Count) highestNumber = ht.Keys.Count;
+            }
+            Console.WriteLine(highestNumber);
+
+            //Lazer {X=30, Y=34}
+            Hashtable htCurrent = new Hashtable();
+            PointF centre = new PointF(30, 34);
+            //PointF centre = new PointF(11, 13);
+            // Start Calculating
+            foreach (PointF item in points)
+            {
+                if (item == centre)
+                    continue;
+
+                Double angle = angleOf(centre, item);
+
+                if (!htCurrent.ContainsKey(angle)){
+                    htCurrent[angle] = new List<PointF>();
+                }
+
+                ((List<PointF>)htCurrent[angle]).Add(item);
+            }
+
+            List<double> sortOrder = new List<double>();
+            foreach (var item in htCurrent.Keys.Cast<double>().Where(dd=>dd<=90).OrderByDescending(d=>d))
+            {
+                sortOrder.Add(item);
+            }
+            foreach (var item in htCurrent.Keys.Cast<double>().Where(dd=>dd<=360 && dd>90).OrderByDescending(d=>d))
+            {
+                sortOrder.Add(item);
+            }
+
+            int itemsKilled = 0;
+            while(htCurrent.Count>0) 
+            {
+                foreach (var item in sortOrder)
+                {
+                    if (!htCurrent.ContainsKey(item))
+                        continue;
+
+                    Hashtable search = new Hashtable();
+                    ((List<PointF>)htCurrent[item]).ForEach(x=>search.Add(ManDist(centre,x),x));
+                    Console.WriteLine(search[search.Keys.Cast<double>().OrderBy(d=>d).First()]);
+                    ((List<PointF>)htCurrent[item]).Remove((PointF)search[search.Keys.Cast<double>().OrderBy(d=>d).First()]);
+                    if (((List<PointF>)htCurrent[item]).Count==0)
+                    {
+                        htCurrent.Remove(item);
+                    }
+                    itemsKilled++;
+                    if (itemsKilled==200) {
+                        Console.WriteLine("Target");
+                    }
+                }
+                
+
+            }
+        }
+
+        //{X=30, Y=34}
+
+
+        /**
+        * Work out the angle from the x horizontal winding anti-clockwise 
+        * in screen space. 
+        * 
+        * The value returned from the following should be 315. 
+        * <pre>
+        * x,y -------------
+        *     |  1,1
+        *     |    \
+        *     |     \
+        *     |     2,2
+        * </pre>
+        * @param p1
+        * @param p2
+        * @return - a double from 0 to 360
+        */
+        public static double angleOf(PointF p1, PointF p2)
+        {
+            // NOTE: Remember that most math has the Y axis as positive above the X.
+            // However, for screens we have Y as positive below. For this reason, 
+            // the Y values are inverted to get the expected results.
+            double deltaY = (p1.Y - p2.Y);
+            double deltaX = (p2.X - p1.X);
+            double result = RadianToDegree(Math.Atan2(deltaY, deltaX));
+            return (result < 0) ? (360d + result) : result;
+        }
+
+        private static double RadianToDegree(double angle)
+        {
+            return angle * (180.0 / Math.PI);
+        }
+
+        private static double ManDist(PointF a, PointF b) {
+            return Math.Abs(a.X - b.X) + Math.Abs(a.Y-b.Y);
+        }
+
+    }
+}
--- Day9/Program.cs
+++ Day9/Program.cs
@@ -9,19 +9,19 @@ namespace Day9
     {
         static void Main(string[] args)
         {
-            string contentsMain = File.ReadAllText(Environment.CurrentDirectory + "/puzzle.txt");
+            string contentsMain = File.ReadAllText(Environment.CurrentDirectory + "/puzzle03.txt");
 
-            int relativeBase = 0;
+            decimal relativeBase = 0;
 
-            decimal runCycle(string contents, decimal input){
+            decimal runCycle(string contents, decimal input)
+            {
 
                 decimal[] array = contents.Split(",").Select(decimal.Parse).ToArray();
+                Array.Resize(ref array, 1000000);
 
-                //array[1] = x;
-                //array[2] = y;
-
-                int[] OptCode(int optCode) {
-                    int[] returnVal = new int[3] {optCode % 10, 0, 0};
+                int[] OptCode(int optCode)
+                {
+                    int[] returnVal = new int[4] { optCode % 100, 0, 0, 0 };
 
                     int length = optCode.ToString().Length;
                     switch (length)
@@ -30,72 +30,126 @@ namespace Day9
                         case 2:
                             break;
                         case 3:
-                            returnVal[1] = (optCode/100) % 10;
+                            returnVal[1] = (optCode / 100) % 10;
                             break;
                         case 4:
-                            returnVal[1] = (optCode/100) % 10;
-                            returnVal[2] = (optCode/1000) % 10;
+                            returnVal[1] = (optCode / 100) % 10;
+                            returnVal[2] = (optCode / 1000) % 10;
+                            break;
+                        case 5:
+                            returnVal[1] = (optCode / 100) % 10;
+                            returnVal[2] = (optCode / 1000) % 10;
+                            returnVal[3] = (optCode / 10000) % 10;
                             break;
                     }
                     return returnVal;
                 }
 
-                void Recursive(int pos){
-
+                int pos = 0;
+                bool NotEnd = false;
+                while (!NotEnd)
+                {
                     int[] optCode = OptCode((int)array[pos]);
+                    decimal val1 = 0, val2 = 0, val3 = 0;
 
-                    decimal val1 = 0, val2 = 0;
-                    if (optCode[0] != 4 && optCode[0] != 3 && optCode[0] != 9) {
-                        val1 = (optCode[1]==0?array[(int)array[pos+1]]:array[pos+1]);
-                        val2 = (optCode[2]==0?array[(int)array[pos+2]]:array[pos+2]);
+                    switch (optCode[1])
+                    {
+                        case 0:
+                            val1 = array[(int)array[pos + 1]];
+                            break;
+                        case 1:
+                            val1 = array[pos + 1];
+                            break;
+                        case 2:
+                            val1 = array[(int)array[pos + 1] + (int)relativeBase];
+                            break;
+                        default:
+                            break;
+                    }
+
+                    switch (optCode[2])
+                    {
+                        case 0:
+                            val2 = array[(int)array[pos + 2]];
+                            break;
+                        case 1:
+                            val2 = array[pos + 2];
+                            break;
+                        case 2:
+                            val2 = array[(int)array[pos + 2] + (int)relativeBase];
+                            break;
+                        default:
+                            break;
+                    }
+
+                    switch (optCode[3])
+                    {
+                        case 0:
+                            val3 = array[pos + 3];
+                            break;
+                        case 2:
+                            val3 = (int)array[pos + 3] + (int)relativeBase;
+                            break;
+                        default:
+                            break;
                     }
 
                     switch (optCode[0])
                     {
                         case 1:
-                            array[(int)array[pos+3]] = val1 + val2;
-                            Recursive(pos+4);
+                            array[(int)val3] = val1 + val2;
+                            pos = (pos + 4);
                             break;
                         case 2:
-                            array[(int)array[pos+3]] = val1 * val2;
-                            Recursive(pos+4);
+                            array[(int)val3] = val1 * val2;
+                            pos = (pos + 4);
                             break;
                         case 3:
-                            array[(int)array[pos+1]] = input;// value
-                            Recursive(pos+2);
+                            if (optCode[1] == 2)
+                            {
+                                array[(int)relativeBase + (int)array[pos + 1]] = input;// value
+                            }
+                            else if (optCode[1] == 1)
+                            {
+                                array[(int)array[pos + 1]] = input;// value
+                            }
+                            pos = (pos + 2);
                             break;
                         case 4:
-                            Console.WriteLine((optCode[1]==0?array[(int)array[pos+1]]:array[pos+1]));
-                            //Console.WriteLine((decimal)array[(int)array[pos+1]]);
-                            Recursive(pos+2);
+                            Console.WriteLine("," + val1);
+                            pos = (pos + 2);
                             break;
                         case 5:
-                            pos = (int)(val1>0?val2:pos+3);
-                            Recursive(pos);
+                            pos = (int)(val1 != 0 ? val2 : pos + 3);
                             break;
                         case 6:
-                            pos = (int)(val1==0?val2:pos+3);
-                            Recursive(pos);
+                            pos = (int)(val1 == 0 ? val2 : pos + 3);
                             break;
                         case 7:
-                            array[(int)array[pos+3]] = (val1<val2?1:0);
-                            Recursive(pos+4);
+                            array[(int)val3] = (val1 < val2 ? 1 : 0);
+                            pos = (pos + 4);
                             break;
                         case 8:
-                            array[(int)array[pos+3]] = (val1==val2?1:0);
-                            Recursive(pos+4);
+                            array[(int)val3] = (val1 == val2 ? 1 : 0);
+                            pos = (pos + 4);
+                            break;
+                        case 9:
+                            relativeBase += val1;
+                            pos = (pos + 2);
                             break;
                         case 99:
-                            return;
+                            NotEnd = true;
+                            break;
+                        default:
+                            Console.WriteLine(optCode[0]);
+                            break;
                     }
                 }
-
-                Recursive(0);
 
                 return input;
             }
 
-            Console.WriteLine($"{runCycle(contentsMain, 5)}");
+            runCycle(contentsMain, 2);
         }
     }
 }
